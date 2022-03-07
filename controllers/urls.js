a37216a676b22f5bddb4d7870878e430a984b263
Melody Davis <piano.fan122@gmail.com>
2022-03-06 19:12:56 -0500

    slight bug fix

--- controllers/urls.js
+++ controllers/urls.js
@@ -1,12 +1,13 @@
 const Url = require('../models/Url')
+const asyncWrapper = require('../middleware/async')
 
-const postOrFindUrl = async (req, res) => {
+const postOrFindUrl = asyncWrapper(async (req, res) => {
     const urlCount = await Url.estimatedDocumentCount()
     const oGUrl = req.body.url
 
     // regex to test if url matches the necessary structure
     if (!/^(https?\:\/\/www\.).+\.\w{2,}/.test(oGUrl)) {
-        res.status(400).json({ error: "invalid url" })
+        return res.status(400).json({ error: "invalid url" })
     }
 
     // if this is not the first url in database
@@ -40,16 +41,16 @@ const postOrFindUrl = async (req, res) => {
         original_url: newUrl.original_url,
         short_url: newUrl.short_url
     })
-}
+})
 
-const goToUrl = async (req, res) => {
+const goToUrl = asyncWrapper (async (req, res) => {
     const { id: shortUrlId } = req.params
     const url = await Url.findOne({ short_url: shortUrlId })
     // checks if url id is valid
     if (!url) {
         return res.status(404).json({ msg: "There is no url associated with that id." })
     }
-    res.status(200).redirect(`${url.original_url}`)
-}
+    res.redirect(`${url.original_url}`)
+})
 
 module.exports = { postOrFindUrl, goToUrl }
